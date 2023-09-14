98d3543066f046425329e7c91bcd22d5713376f5
Lari Löfman <larwazor@gmail.com>
2023-09-14 19:09:51 +0300

    fix name plates for FN

--- VirtualWorld/Assets/Scripts/UI/NamePlate.cs
+++ VirtualWorld/Assets/Scripts/UI/NamePlate.cs
@@ -5,37 +5,55 @@ using TMPro;
 using Unity.Collections;
 using FishNet.Object;
 using UnityEngine;
+using FishNet.Object.Synchronizing;
 
 namespace Authentication
 {
     public class Nameplate : NetworkBehaviour
     {
-        [SerializeField] TMP_Text namePlate;
-        string username;
+        [SerializeField] TMP_Text nameplate;
+        [SyncVar] string username;
 
         private void Awake()
         {
-            if (namePlate == null)
+            if (nameplate == null)
             {
-                namePlate = GetComponentInChildren<TMP_Text>();
+                nameplate = GetComponentInChildren<TMP_Text>();
             }
         }
 
-        void Start()
+        //private void Start()
+        //{
+        //    nameplate.text = username;
+        //}
+
+        public override void OnStartClient()
         {
-            if (namePlate != null && UserSession.Instance.LoggedUserData.username != null)
+            base.OnStartClient();
+
+            if (nameplate != null)
             {
-                //if (IsOwner)
-                //{
-                //    username = UserSession.Instance.LoggedUserData.username;
-                //}
-                //else
+                if (IsOwner && IsClient)
                 {
-                    namePlate.text = username;
+                    SetNameServerRpc(UserSession.Instance.LoggedUserData.username);
+                    nameplate.text = UserSession.Instance.LoggedUserData.username;
+                } else
+                {
+                    nameplate.text = username;
                 }
             }
         }
 
-        
+        [ServerRpc]
+        void SetNameServerRpc(string name)
+        {
+            username = name;
+            SetNameObserversRpc(name);
+        }
+        [ObserversRpc]
+        void SetNameObserversRpc(string name)
+        {
+            nameplate.text = name;
+        }
     }
 }
